63057beeebb8ed43d0871203ec2c18559ff2abcb
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-08-19 02:30:46 -0300

    changed to test visor - more tests

--- python/tests.py
+++ python/tests.py
@@ -1,8 +1,6 @@
 import unittest
 from calculator import *
 
-
-
 class TestCalculatorFunctions(unittest.TestCase):
 
     def test_input_A(self):
@@ -68,7 +66,7 @@ class TestCalculatorFunctions(unittest.TestCase):
         calc.store_value("+")
         calc.input(5)
         calc.get_result()
-        self.assertEqual(calc.result, 8.0)
+        self.assertEqual(calc.visor, 8.0)
     
     def test_carry_via_result(self):
         calc = CalculatorLogic()
@@ -77,21 +75,23 @@ class TestCalculatorFunctions(unittest.TestCase):
         calc.get_result()
         calc.get_result()
         calc.get_result()
-        self.assertEqual(calc.result, 81.0)
+        self.assertEqual(calc.visor, 81.0)
 
     def test_carry_via_operator(self):
         calc = CalculatorLogic()
         calc.input(1)
+        calc.input(1)
         calc.store_value("+")
         calc.input(9)
         calc.store_value("+")
-        self.assertEqual(calc.result, 10.0)
+        self.assertEqual(calc.visor, 20.0)
         calc.input(2)
-        calc.store_value("+")
-        self.assertEqual(calc.result, 12.0)
+        calc.input(2)
+        calc.store_value("-")
+        self.assertEqual(calc.visor, 42.0)
         calc.input(5)
         calc.store_value("+")
-        self.assertEqual(calc.result, 15.0)
+        self.assertEqual(calc.visor, 37.0)
     
     def test_carry_operation_change_operator(self):
         calc = CalculatorLogic()
@@ -100,7 +100,7 @@ class TestCalculatorFunctions(unittest.TestCase):
         calc.get_result()
         calc.store_value("-")
         calc.get_result()
-        self.assertEqual(calc.result, 0.0)
+        self.assertEqual(calc.visor, 0.0)
 
     def test_output_int(self):
         calc = CalculatorLogic()
@@ -108,7 +108,7 @@ class TestCalculatorFunctions(unittest.TestCase):
         calc.store_value("/")
         calc.input(2)
         calc.get_result()
-        self.assertEqual(calc.result, 8)
+        self.assertEqual(calc.visor, 8)
     
     def test_output_float(self):
         calc = CalculatorLogic()
@@ -116,7 +116,44 @@ class TestCalculatorFunctions(unittest.TestCase):
         calc.store_value("/")
         calc.input(2)
         calc.get_result()
-        self.assertEqual(calc.result, 2.5)
+        self.assertEqual(calc.visor, 2.5)
+
+    def test_two_operations(self):
+        calc = CalculatorLogic()
+        calc.input(3)
+        calc.store_value("*")
+        calc.input(5)
+        calc.get_result()
+        calc.input(3)
+        calc.store_value("*")
+        calc.input(5)
+        calc.get_result()
+        self.assertEqual(calc.visor, 15.0)
+
+    def test_clear_all(self):
+        calc = CalculatorLogic()
+        calc.input(3)
+        calc.store_value("*")
+        calc.input(5)
+        calc.get_result()
+        calc.clear_all()
+        calc.input(3)
+        calc.input(3)
+        calc.store_value("*")
+        calc.input(5)
+        calc.get_result()
+        self.assertEqual(calc.visor, 165.0)
+    
+    def test_clear_last(self):
+        calc = CalculatorLogic()
+        calc.input(3)
+        calc.store_value("*")
+        calc.input(5)
+        calc.clear_last()
+        calc.input(3)
+        calc.get_result()
+        self.assertEqual(calc.visor, 9)
+
 
 if __name__ == '__main__':
     unittest.main()
