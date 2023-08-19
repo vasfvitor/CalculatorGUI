f458244970719fa0bb928631274e57911ad03393
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-08-19 02:31:10 -0300

    parse results to float or int - catch errors

--- python/operations.py
+++ python/operations.py
@@ -1,13 +1,40 @@
+import math
+
+
 def add(x, y):
-    return float((x) + (y))
+    try:
+        res = float(float(x) + float(y))
+        out = int(res) if res.is_integer() else res
+    except:
+        print("ERROR: ADDITION")
+    return out
+
 
 def subtract(x, y):
-    return float((x) - (y))
+    try:
+        res = float(float(x) - float(y))
+        out = int(res) if res.is_integer() else res
+        return out
+    except:
+        print("ERROR: SUBTRACTION")
+
 
 def multiply(x, y):
-    return float((x) * (y))
+    try:
+        res = float(float(x) * float(y))
+        out = int(res) if res.is_integer() else res
+        return out
+    except:
+        print("ERROR: MULTIPLY")
+
 
 def divide(x, y):
-    if float(y) == 0.0:
-        print("Can't be")
-    return float((x) / (y))
+    try:
+        if float(y) == 0.0:
+            print("Can't be")
+            return "Can't divide by 0"
+        res = float(float(x) / float(y))
+        out = int(res) if res.is_integer() else res
+        return out
+    except:
+        print("ERROR: DIVISION")
